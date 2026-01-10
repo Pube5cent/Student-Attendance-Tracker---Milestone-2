3d0a36a573d1bf7bce4ff83ea1c1dd93027f2fee
Adam <adamsyafiqsallehuddin@gmail.com>
2026-01-10 14:58:36 +0800

    Proper File name

---
+++ TC5L_Group7_main.cpp
@@ -0,0 +1,15 @@
+// *********************************************************
+// Program: Student Attendance Tracker - Milestone 2
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC5L
+// Tutorial Class: TT15L
+// Trimester: 2530
+// Member_1: 252UC242D2 | ADAM SYAFIQ BIN SALLEHUDDIN | ADAM.SYAFIQ.SALLEHUDDIN1@STUDENT.MMU.EDU.MY | 012-361 3065
+// Member_2: 252UC242QY | MUHAMMAD HAFIY AL-HAFIIZH BIN JOAHARI | MUHAMMAD.HAFIY.ALHAFIIZH1@STUDENT.MMU.EDU.MY | 016-212 3859
+// Member_3: 252UC242NE | AIDAN KHALIQ BIN SHAHNAZARUDDIN | AIDAN.KHALIQ.SHAHNZARUDDI1@student.mmu.edu.my | 017-615 5250
+// *********************************************************
+// Task Distribution
+// Member_1: 
+// Member_2: 
+// Member_3: 
+// *********************************************************
